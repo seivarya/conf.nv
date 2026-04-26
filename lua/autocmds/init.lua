ae8262534d2abed61e81b0fdcace23777fe4dcd0
seivarya <seivarya.he@gmail.com>
2026-04-26 21:43:09 +0530

    [feat]: transparent bg/ etc etc

--- lua/autocmds/init.lua
+++ lua/autocmds/init.lua
@@ -14,3 +14,8 @@ vim.api.nvim_create_autocmd('FileType', {
 	pattern = 'netrw',
 	command = 'setlocal colorcolumn=',
 })
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none"})
--- lua/plugs/ui/colorscheme.lua
+++ lua/plugs/ui/colorscheme.lua
@@ -5,7 +5,7 @@ return {
 		priority = 1000,
 		opts = {
 			flavour = "macchiato",
-
+			transparent_background = true,
 			styles = {
 				comments = {},
 				conditionals = {},
